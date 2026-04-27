6ec6860c6918bb9f41f584d64759e4d08998ab88
leolyoo-mbc <leolyoo.mbc@gmail.com>
2026-04-27 16:16:13 +0900

    Add SampleScene & HelloPractice; bump Unity
    Add a new Unity scene (Assets/SampleScene.unity) containing a Camera, Directional Light and Cube with HelloWorld and new HelloPractice components. Add HelloPractice.cs which logs "Hello, Game" and "123456789" on Start. Update HelloWorld.cs to include explanatory comments. Bump ProjectSettings/ProjectVersion.txt to Unity 6000.3.14f1.

---
+++ Assets/Scripts/HelloUnity/HelloPractice.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloPractice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, Game");
+        Debug.Log("123456789");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
